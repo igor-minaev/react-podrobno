2441cdb2ad0a9e68aa50ef07ede96a6d1bec51e9
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-22 20:34:36 +0300

    start props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {Accordion} from './components/Accordion/Accordion';
 
 function hello() {
     alert('Hello')
@@ -40,32 +41,6 @@ const Rating = () => {
 function Star() {
     console.log('Star rendering')
     return <div>star</div>
-}
-
-const Accordion = () => {
-    console.log('Accordion rendering')
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    )
-}
-
-function AccordionTitle() {
-    console.log('AccordionTitle rendering')
-    return <h1>Menu</h1>
-}
-
-function AccordionBody() {
-    console.log('AccordionBody rendering')
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
 }
 
 
