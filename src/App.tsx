85a1af3ef9f105f0249634e321dbe82230ae2a80
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-22 19:30:52 +0300

    components 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,24 @@ function hello() {
 // hello()
 
 function App() {
+    console.log('App rendering')
     return (
         <div className="App">
             <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
 }
 
-function AppTitle(){
+function AppTitle() {
+    console.log('AppTitle rendering')
     return <>This is APP component</>
 }
 
 const Rating = () => {
+    console.log('Rating rendering')
     return (
         <div>
             <Star/>
@@ -34,20 +38,35 @@ const Rating = () => {
 }
 
 function Star() {
+    console.log('Star rendering')
     return <div>star</div>
 }
 
 const Accordion = () => {
+    console.log('Accordion rendering')
     return (
         <div>
-            <h1>Menu</h1>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
     )
 }
+
+function AccordionTitle() {
+    console.log('AccordionTitle rendering')
+    return <h1>Menu</h1>
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering')
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
 
 export default App;
