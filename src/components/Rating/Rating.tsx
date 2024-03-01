3894c0e9efb90525000f9aeca6528d0627c996b6
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-01 18:52:15 +0300

    uncontrolled components

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -24,9 +24,10 @@ type StarPropsType = {
 
 function Star(props: StarPropsType) {
     console.log('Star rendering')
-    if (props.selected) {
-        return <span><b>star</b> </span>
-    } else {
-        return <span>star </span>
-    }
+    // if (props.selected) {
+    //     return <span><b>star</b> </span>
+    // } else {
+    //     return <span>star </span>
+    // }
+    return <span>{props.selected ? <b>star</b> : 'star'}</span>
 }
