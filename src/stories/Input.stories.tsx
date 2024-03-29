abce22231eada5496517032cbcf530702507ef08
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-29 10:59:46 +0300

    start uncontrolled input

---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,10 @@
+
+
+export default {
+    title: 'input'
+}
+
+export const UncontrolledInput = () => <input/>
+export const TrackValueOfUncontrolledInput = () => <input/>
+
+export const ControlledInputWithFixedValue = () => <input value='it-incubator'/>
